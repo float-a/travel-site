bf4fafbe702ef993f25465ed3a88564a1100bf35
Adrian <adrian.ardelean95@gmail.com>
2016-11-29 17:51:03 +0200

    Finish animating menu icon.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -96,6 +96,7 @@
 	            console.log(this);
 	            this.menuContent.toggleClass("site-header__menu-content--is-visible");
 	            this.siteHeader.toggleClass("site-header--is-expanded");
+	            this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 	        }
 	    }]);
 
