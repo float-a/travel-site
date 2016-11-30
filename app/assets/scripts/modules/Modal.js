2998bd2b280658eb73afddb4ca4ef446b731a83e
Adrian <adrian.ardelean95@gmail.com>
2016-11-30 15:00:58 +0200

    Completed the modal overlay.

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+
+export default class Modal {
+    constructor(){
+        this.openModalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.events();
+    }
+
+    events() {
+        // clicking the open modal button 
+        this.openModalButton.click(this.openModal.bind(this));
+
+        // clicking the x close modal button
+        this.closeModalButton.click(this.closeModal.bind(this));
+
+        //pushes any key
+        $(document).keyup(this.keyPressHandler.bind(this));
+    }
+
+    keyPressHandler(e){
+        if(e.keyCode == 27) {
+            this.closeModal();
+        }
+    }
+
+    openModal() {
+        this.modal.addClass("modal--is-visible");
+        return false; // prevents scrolling up when you click the Get in touch button because of href=#
+    }
+
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+    }
+}
